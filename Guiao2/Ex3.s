c08cfc720a4f02c000b0c36d9f77470e344b6a66
Daniel <dani.andrade@ua.pt>
2020-03-04 17:01:15 +0000

    ex3 concluido

--- Guiao2/Ex3.s
+++ Guiao2/Ex3.s
@@ -8,13 +8,15 @@
 .globl main
 
 main:   lui $t1,0xBF88
-        lw $t2,0x6050($t1)
 
-while : li $v0,READ_CORE_TIMER 				  # while (1) {
+while : lw $t2,0x6050($t1)
+        li $v0,READ_CORE_TIMER 				  # while (1) {
         syscall 					              #
         blt $v0,10000000,while
+        li $v0,RESET_CORE_TIMER
+        syscall
 
-        move $t2,$a0
+        andi $a0,$t2,0x0000000F
         li $a1,0x00040002
         li $v0,PRINT_INT
         syscall
