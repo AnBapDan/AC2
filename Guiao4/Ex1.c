b36a526aa092488c83afc948a8c90a901779711a
Daniel <dani.andrade@ua.pt>
2020-03-05 09:56:39 +0000

    changes

--- Guiao4/Ex1.c
+++ Guiao4/Ex1.c
@@ -1,4 +1,26 @@
 #include <detpic32.h>
+typedef struct {
+ unsigned int TRISE0 : 1; // 1-bit field (least significant bit)
+ unsigned int TRISE1 : 1; // ...
+ unsigned int TRISE2 : 1; // ...
+ unsigned int TRISE3 : 1; // ...
+ unsigned int TRISE4 : 1; // ...
+ unsigned int TRISE5 : 1; // ...
+ unsigned int TRISE6 : 1; // ...
+ unsigned int TRISE7 : 1; // 1-bit field (most significant bit)
+ } __TRISEbits_t;
+
+
+typedef struct {
+ unsigned int LATE0 : 1;
+ unsigned int LATE1 : 1;
+ unsigned int LATE2 : 1;
+ unsigned int LATE3 : 1;
+ unsigned int LATE4 : 1;
+ unsigned int LATE5 : 1;
+ unsigned int LATE6 : 1;
+ unsigned int LATE7 : 1;
+} __LATEbits_t;
 
 void delay(unsigned int ms)
 {
@@ -10,12 +32,12 @@ void delay(unsigned int ms)
   }
 }
   int main(void){
-   LATDbits.LATD0 = 0; // The initial value should be set
+   LATEbits.LATE0 = 0; // The initial value should be set
    // before configuring the port as output
-   TRISDbits.TRISD0 = 0; // RD0 configured as output
+   TRISEbits.TRISE0 = 0; // RD0 configured as output
    while(1){
      delay(500); // half period = 0.5s
-     LATDbits.LATD0 = !LATDbits.LATD0;
+     LATEbits.LATE0 = !LATEbits.LATE0;
    }
    return 0;
   }
